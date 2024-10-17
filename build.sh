eb96dcbf44350d32592d1d12b7f0c852df022f2b
Joseph Hanson <joe@veri.dev>
2024-10-16 20:41:20 -0500

    add webui into build and update build script

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ VERSION='5.0.0'
 DATE=$(date -I)
 REGISTRY='ghcr.io/jahanson'
 TAG="v${VERSION}"
+WEBUI_VERSION="2.1.0"
+TARGETPLATFORM='linux/amd64'
+# currently does nothing but set the version property when building the dotnet app.
+CHANNEL='stable'
 
 REPO_URL="https://github.com/ShokoAnime/ShokoServer.git"
 
@@ -16,11 +20,12 @@ echo "Short commit hash for tag ${TAG}: ${COMMIT_HASH}"
 
 # Build the Docker image for ShokoServer
 docker build \
-    --build-arg VERSION="${VERSION}" \
-    --build-arg DATE="${DATE}" \
+    --build-arg CHANNEL="${CHANNEL}" \
     --build-arg COMMIT="${COMMIT_HASH}" \
-    --build-arg TARGETPLATFORM='linux/amd64' \
-    --build-arg CHANNEL='stable' \
+    --build-arg DATE="${DATE}" \
     --build-arg TAG="${TAG}" \
+    --build-arg TARGETPLATFORM="${TARGETPLATFORM}" \
+    --build-arg VERSION="${VERSION}" \
+    --build-arg WEBUI_VERSION="${WEBUI_VERSION}" \
     -t ${REGISTRY}/shokoserver:${TAG} . \
     -f ./apps/ShokoServer/Dockerfile
